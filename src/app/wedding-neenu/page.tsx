39f11e4f5900879e331a737273eab4385699cd25
Deepak <deepakmehra149@gmail.com>
2025-09-20 14:27:59 +0530

    aded card

--- src/app/wedding-neenu/page.tsx
+++ src/app/wedding-neenu/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
 
 const WeddingNeenu: React.FC = () => {
